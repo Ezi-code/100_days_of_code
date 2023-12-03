d79cade962259bb43a198ba2a6b47d24751786c4
Ezra Yendau <ezrayendau@gmail.com>
2023-12-03 17:54:54 +0000

    done witha day 8

---
+++ day007/day007.py
@@ -0,0 +1,3 @@
+string = input("enter a string: ")
+
+print(string[::-1])
---
+++ day008/day008.py
@@ -0,0 +1,7 @@
+def factorial(n):
+   fact = 1
+   for num in range(2, n + 1):
+       fact *= num
+   return print(fact)
+
+factorial(4)
