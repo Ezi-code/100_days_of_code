a2b8c1afb27f626feea21a0270d994ffd3d55e15
Ezra Yendau <ezrayendau@gmail.com>
2023-12-04 00:12:39 +0000

    done with day 9

---
+++ day009/day009.py
@@ -0,0 +1,12 @@
+first_number = float(input("Enter first number: "))
+second_number = float(input("Enter second number: "))
+operator = input("enter operator: ")
+
+if operator == '+':
+    print(first_number + second_number)
+if operator == '-':
+    print(first_number - second_number)
+if operator == '*':
+    print(first_number * second_number)
+if operator == '/':
+    print(first_number / second_number)
